55b136021e42de3c6edd3e3562e31a9024ab221b
Evan Reid <evan_1995@hotmail.com>
2020-11-06 18:15:15 +0000

    4th change, fixed 3rd problem

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -7,7 +7,13 @@ def get_pet_shop_name(input_dictionary):
 def get_total_cash(input_dictionary):
     return input_dictionary["admin"]["total_cash"]
 
+
 def add_or_remove_cash(input_dict, amount_to_add):
-    final_amount = input_dict["admin"]["total_cash"] + amount_to_add
-    return final_amount
+    final_amount = input_dict["admin"]["total_cash"] + amount_to_add 
+    add_final_amount_to_dict = input_dict["admin"]["total_cash"] = final_amount
+    return add_final_amount_to_dict
     
+def add_or_remove_cash(input_dict, amount_to_remove):
+    final_amount_sub = input_dict["admin"]["total_cash"] + amount_to_remove
+    remove_final_amount_from_dict = input_dict["admin"]["total_cash"] = final_amount_sub
+    return remove_final_amount_from_dict
