b51c84daca80877a41f035a98e679b1fb15bb47d
Evan Reid <evan_1995@hotmail.com>
2020-11-06 17:29:11 +0000

    second problem

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -3,3 +3,6 @@
 def get_pet_shop_name(input_dictionary):
     result = input_dictionary["name"]
     return result
+
+def get_total_cash(input_dictionary):
+    return input_dictionary["admin"]["total_cash"]
